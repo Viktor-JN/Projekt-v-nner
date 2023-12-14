070062326e9a49fe2db4c5b58cb0c9efde2efa7f
Viktor-JN <viktor.johanssonnygren@elev.ntig.se>
2023-12-14 12:57:06 +0100

    added comments

--- friends.py
+++ friends.py
@@ -9,7 +9,7 @@ __email__   = "viktor.jonanssonnygren@elev.ga.ntig.se"
 from msvcrt import getwch
 import os
 from time import sleep
-def remove():
+def remove(): #funktion för att ta bort saker från en lista
     y=1
     while True:
         for x in ancestry:
@@ -26,24 +26,29 @@ def remove():
     except:
         print("Skriv nummret av namnet du vill ta bort")
 
-def add():
+def add(): #funktion för att lägga till saker i listan
     x=input("Vad vill du lägga till i släktträdet\n")
     os.system('cls')
-    if x in ancestry:
-        print(f"""{x} finns redan i ditt släktträd, vill du ändå lägga till det?
+    if x in ancestry: #kollar om användarens val finns i listan
+        while True:
+            print(f"""{x} finns redan i ditt släktträd, vill du ändå lägga till det?
 Ja(Y)
 Nej(N)""")
-        answer=getwch().upper()
-        if answer == "Y":
-            os.system('cls')
-            ancestry.append(x)
-        elif answer == "N":
-            os.system('cls')
+            answer=getwch().upper() #kollar om användaren 
+            if answer == "Y":
+                os.system('cls')
+                ancestry.append(x)
+                break
+            elif answer == "N":
+                os.system('cls')
+                break
+            else:
+                print("Y eller N")
     else:
         ancestry.append(x)
 
-def sortera():
-    if len(ancestry) == 0:
+def sortera(): #funktion för att sortera listan
+    if len(ancestry) == 0: #kollar ifall listan är tom
         print("Du kan inte sortera en tom lista")
         sleep(2)
     else:
@@ -57,45 +62,44 @@ def sortera():
         print("Listan sorteras...")
         sleep(1)
         os.system('cls')
-        ancestry.sort()
-        os.system('cls')
+        ancestry.sort() #sorterar listan
 
-ancestry=[]
+ancestry=[] #skapar listan som man lägger in användarens val i
 
 while True:
     os.system('cls')
     nummer=1
     print("Ditt släktträd")
-    for x in ancestry:
+    for x in ancestry: #skriver ut hela listan med allt innehåll och vilken position de har i släktträdet
         print(nummer, x)
         nummer+=1
     print("")
-    print(f"Du har {nummer-1} personer i ditt släktträd")
+    print(f"Du har {nummer-1} personer i ditt släktträd") #skriver ut hur många personer man har i släktträdet
     print("")
     print("""Hur vill du redigera ditt släktträd
 Lägg till/a
 Ta bort/r
 Sortera/s
 Avsluta/q\n""")
-    y=getwch().upper()
+    y=getwch().upper() #tar användarens knapptryck så de kan välja vad de vill göra
     if y.upper() == "A":
         os.system('cls')
-        add()
+        add() #starta funktionen för att lägga till något i listan
     elif y.upper() == "R":
-        if len(ancestry) == 0:
+        if len(ancestry) == 0: #kollar om listan är tom så när någon ska ta bort
             os.system('cls')
             print("Du kan inte ta bort från en lista som inte har någonting i sig")
             sleep(2)
         else:
             os.system('cls')
-            remove()
+            remove() #starta funktionen för att ta bort någonting från listan
     elif y.upper() == "Q":
         print("Hej då!")
-        break
-    elif y.upper() == "S":
+        exit()
+    elif y.upper() == "S": 
         os.system('cls')
-        sortera()
-    else:
+        sortera() #starta funktionen för att sortera listan
+    else: #om man klickar på en knapp som inte gör något så får man välja igen
         os.system('cls')
         print("Välj någonting från listan")
         sleep(2)
